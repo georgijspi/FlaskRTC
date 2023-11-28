2460ebcf3c3fe1fc1ead86966554e216aa481095
Georgijs Pitkevics <georgijs.pitkevics2@mail.dcu.ie>
2023-11-28 19:32:16 +0000

    Edited some issues.

--- forms.py
+++ forms.py
@@ -1,14 +1,10 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, SelectField
-from wtforms.validators import DataRequired, Length
+from wtforms import StringField, PasswordField, validators
 
 class RegistrationForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired(), Length(min=4, max=25)])
-    password = PasswordField('Password', validators=[DataRequired()])
-    confirm_password = PasswordField('Confirm Password', validators=[DataRequired()])
-    submit = SubmitField('Register')
-
-class ChatRoomForm(FlaskForm):
-    room_name = StringField('Room Name', validators=[DataRequired()])
-    visibility = SelectField('Visibility', choices=[('public', 'Public'), ('private', 'Private')], validators=[DataRequired()])
-    submit = SubmitField('Create Chat Room')
+    username = StringField('Username', [validators.Length(min=4, max=25)])
+    password = PasswordField('Password', [
+        validators.DataRequired(),
+        validators.EqualTo('confirm', message='Passwords must match')
+    ])
+    confirm = PasswordField('Repeat Password')
